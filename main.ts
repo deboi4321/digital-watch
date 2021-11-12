01d2247e9aa53fda6552bd0f7281a392dbd2e458
deboi4321 <93968683+deboi4321@users.noreply.github.com>
2021-11-11 22:05:12 -0800

    Update main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -5,6 +5,32 @@ input.onButtonPressed(Button.A, function () {
         hours = 0
     }
 })
+input.onSound(DetectedSound.Loud, function () {
+    adjust = hours
+    if (ampm) {
+        if (hours > 12) {
+            adjust = hours - 12
+        } else {
+            if (hours == 0) {
+                adjust = 12
+            }
+        }
+    }
+    time = "" + adjust
+    time = "" + time + ":"
+    if (minutes < 10) {
+        time = "" + time + "0"
+    }
+    time = "" + time + minutes
+    if (ampm) {
+        if (hours > 11) {
+            time = "" + time + "PM"
+        } else {
+            time = "" + time + "AM"
+        }
+    }
+    basic.showString(time)
+})
 input.onButtonPressed(Button.AB, function () {
     ampm = !(ampm)
 })
@@ -15,7 +41,7 @@ input.onButtonPressed(Button.B, function () {
         minutes = 0
     }
 })
-input.onGesture(Gesture.Shake, function () {
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
     adjust = hours
     if (ampm) {
         if (hours > 12) {
